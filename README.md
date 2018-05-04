46bdf1da2ff0763aa611fe9a9023d1c8cb8b7f89
atlroc99 <jeffry.zaman>
2018-05-04 13:20:10 -0400

    updated readme file

---
+++ README.md
@@ -0,0 +1,7 @@
+successfully connected to the progresql database.
+define the default schema in the mapping xml file. 
+<hibernate-mapping>
+    <class name="com...your.classname" schema="schemaname">
+    ..
+</hibernate-mapping>
+
--- justHibernate/src/main/java/org/gsu/justHibernate/connection/Connection.java
+++ justHibernate/src/main/java/org/gsu/justHibernate/connection/Connection.java
@@ -6,24 +6,20 @@ import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 
 public class Connection {
-	
+
 	private static SessionFactory sessionFactory;
-	
-	private static void setUp () throws Exception
-	{
-		final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
-														.configure("hibernate.cfg.xml")
-														.build();
-		
+
+	private static void setUp() throws Exception {
+		final StandardServiceRegistry registry = new StandardServiceRegistryBuilder().configure("hibernate.cfg.xml")
+				.build();
 		try {
 			sessionFactory = new MetadataSources(registry).buildMetadata().buildSessionFactory();
-			
-		}catch(Exception e) {
+
+		} catch (Exception e) {
 			StandardServiceRegistryBuilder.destroy(registry);
 		}
 	}
-	
-	
+
 	public static SessionFactory getSessionFactory() throws Exception {
 		setUp();
 		return sessionFactory;
